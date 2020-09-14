05d3a82c479ade19bdf50f8e9f7fad833dfb9680
Pro <pro@Pros-MacBook-Pro.local>
2020-09-14 23:28:53 +0800

    refactiration del afonction componet et lajout de lapi de geolocalisation

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class App extends React.Component {
+	render() {
+		window.navigator.geolocation.getCurrentPosition((position) => console.log(position), (err) => console.log(err));
+		return <div>latitude :</div>;
+	}
+}
+ReactDom.render(<App />, document.querySelector('#root'));
