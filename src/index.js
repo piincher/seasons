486258611e65511e8c5f35b7ac7ad8530e6eae6f
Pro <pro@Pros-MacBook-Pro.local>
2020-09-15 21:09:40 +0800

    add many stuff to our application icon check the status of the wether ......

--- src/index.js
+++ src/index.js
@@ -1,29 +1,23 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import SeasonDisplay from './Season.component';
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
+	state = { lat: null, errorMessage: '' };
 
-		this.state = {
-			lat: null,
-			errorMessage: ''
-		};
+	componentDidMount() {
 		window.navigator.geolocation.getCurrentPosition(
-			(position) => {
-				this.setState({ lat: position.coords.latitude });
-			},
-			(err) => {
-				this.setState({ errorMessage: err.message });
-			}
+			(position) => this.setState({ lat: position.coords.latitude }),
+			(err) => this.setState({ errorMessage: err.message })
 		);
 	}
+
 	render() {
 		if (this.state.errorMessage && !this.state.lat) {
 			return <div>error : {this.state.errorMessage}</div>;
 		}
 		if (!this.state.errorMessage && this.state.lat) {
-			return <div> latitude : {this.state.lat}</div>;
+			return <SeasonDisplay lat={this.state.lat} />;
 		}
 		return <div>loading !</div>;
 	}
